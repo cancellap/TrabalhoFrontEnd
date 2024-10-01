8d118aeeac9a1358bdc866effe8c1cc7d892a27b
Pedro Cancella Oliveira <pedrocancella94@gmail.com>
2024-10-01 17:58:44 -0300

    Ativando traducao da piada

--- js/testeApi.js
+++ js/testeApi.js
@@ -8,11 +8,11 @@ document.getElementById("botao-piada").addEventListener("click", async () => {
         const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${sortCatego()}`);
 
         const jokeData = await response.json();
-        // const joke = jokeData.value;
-        // const traducaoPiada = await traduzirTexto(joke, 'en', 'pt');
-        // h1.textContent = `${traducaoPiada}`;
-        h1.textContent = jokeData.value;
-        // audio.play();
+        const joke = jokeData.value;
+        const traducaoPiada = await traduzirTexto(joke, 'en', 'pt');
+        h1.textContent = `${traducaoPiada}`;
+        // h1.textContent = jokeData.value;
+        audio.play();
     } catch (e) {
         console.log("Erro ao buscar a piada");
     }
@@ -25,26 +25,26 @@ function sortCatego() {
     return catego[num];
 }
 
-// async function traduzirTexto(texto, idiomaOrigem, idiomaDestino) {
-//     const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=${idiomaOrigem}|${idiomaDestino}`;
+async function traduzirTexto(texto, idiomaOrigem, idiomaDestino) {
+    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=${idiomaOrigem}|${idiomaDestino}`;
 
-//     try {
-//         const response = await fetch(url);
-//         if (!response.ok) {
-//             throw new Error(`Erro na requisição da api MyMemory`);
-//         }
-//         const data = await response.json();
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Erro na requisição da api MyMemory`);
+        }
+        const data = await response.json();
 
 
-//         if (data && data.responseData && data.responseData.translatedText) {
-//             return data.responseData.translatedText;
-//         } else {
-//             throw new Error("Tradução não disponível");
-//         }
-//     } catch (error) {
-//         console.error("Erro:", error);
-//         return "Erro ao traduzir a piada.";
-//     }
-//     console.log(data);
-// }
+        if (data && data.responseData && data.responseData.translatedText) {
+            return data.responseData.translatedText;
+        } else {
+            throw new Error("Tradução não disponível");
+        }
+    } catch (error) {
+        console.error("Erro:", error);
+        return "Erro ao traduzir a piada.";
+    }
+    console.log(data);
+}
 
